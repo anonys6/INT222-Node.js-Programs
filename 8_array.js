15e5ae0640b38f41568c879a0a792520a157886c
anonys6 <shakibalam998@gmail.com>
2024-02-12 09:52:05 +0530

    adding medthods of array

---
+++ 8_array.js
@@ -0,0 +1,45 @@
+const courses = ['JavaScript', "NodeJS", "ReactJS", , , , , ,];
+
+console.log(courses);
+console.log(courses[3]);
+
+// destructuring an array
+let [x, ...y] = courses;
+
+console.log(x + " " + y);
+
+
+const num = [1, 2, 3, 4, 5];
+console.log(num);
+num.push(6);
+console.log("After pushing 6 in num: " + num);
+console.log("Lenght of num: " + num.length);
+
+const num1 = [7, 8, 9];
+const num2 = [10, 11, 12];
+const num3 = [13, 14, 15];
+
+console.log(num1.concat(num2, num3));
+// num1 = num1.concat(num2, num3); // use let to do this
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num2 + "," + num3);
+
+const num4 = num1.concat(num2, num3);
+console.log(num4);
+const even = num4.every(function(e) {
+    return e % 2 === 0;
+});
+console.log(even);
+
+const odd = num4.filter(function(e) {
+    return e % 2 !== 0;
+});
+console.log(odd);
+
+num3.forEach(function(num, i) {
+    num3[i] = num3[i] ** 2;
+    console.log(num);
+});
+console.log(num3);
