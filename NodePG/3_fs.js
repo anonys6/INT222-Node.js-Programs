5d42cd9043a5cc75709be558505e79dd4983725b
anonys6 <shakibalam998@gmail.com>
2024-02-19 23:01:43 +0530

    file system (fs) done

--- NodePG/3_fs.js
+++ NodePG/3_fs.js
@@ -3,7 +3,9 @@ async handles result
 async doesn't returns anything
 */
 
+const { log } = require('console');
 const fs = require('fs');
+const { clearScreenDown } = require('readline');
 
 fs.writeFileSync('./NodePG/files/3_fs_sync.txt', 'Hello World from fs.writeFileSync');
 
@@ -14,3 +16,21 @@ fs.writeFile('./NodePG/files/3_fs_async.txt', 'Hello from async', (err) => {
         console.log("found no error");
     }
 })
+
+const syncString = fs.readFileSync("./NodePG/files/3_fs_sync.txt", "utf-8");
+console.log(syncString);
+
+fs.readFile('./NodePG/files/3_fs_async.txt', 'utf-8', (err, str) => {
+    if (err) {
+        console.log("error found: ", err);
+    } else {
+        console.log("Print result");
+        console.log(str);
+    }
+})
+
+fs.appendFileSync('./NodePG/files/3_fs_sync.txt', `\n${Date.now()}: Hey There}\n`);
+
+fs.cpSync('./NodePG/files/3_fs_sync.txt', './NodePG/files/3_cp_file.txt');
+fs.unlinkSync('./NodePG/files/3_cp_file.txt');
+
--- NodePG/tempCodeRunnerFile.js
+++ NodePG/tempCodeRunnerFile.js
@@ -1,1 +1,8 @@
-math
+
+fs.readFile('./NodePG/files/3_fs_async.txt', 'utf-8', (err, str) => {
+    if (err) {
+        console.log("error found: ", err);
+    } else {
+        console.log("Print result");
+        console.log(str);
+    }
