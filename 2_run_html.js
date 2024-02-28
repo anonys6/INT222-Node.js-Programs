7c41e99fb75da05e5e3fb95e078701cf2a52ff08
anonys6 <shakibalam998@gmail.com>
2024-02-28 08:36:45 +0530

    done 2

--- 2_run_html.js
+++ 2_run_html.js
@@ -23,3 +23,31 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+
+
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
+
+// const hostname = "127.0.0.1";
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     const filePath = path.join(__dirname, "index.html");
+
+//     fs.readFile(filePath, (err, content) => {
+//         if (err) {
+//             res.statusCode = 500;
+//             res.setHeader("Content-Type", "text/plain");
+//             res.end("An error has occurred");
+//         } else {
+//             res.statusCode = 200;
+//             res.setHeader("Content-Type", "text/html");
+//             res.end(content);
+//         }
+//     })
+// })
+
+// server.listen(port);
