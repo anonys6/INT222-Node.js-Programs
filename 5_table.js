b74751f87f4939cf3ad2bca508848da977862375
anonys6 <shakibalam998@gmail.com>
2024-02-28 08:57:14 +0530

    done 6

--- 5_table.js
+++ 5_table.js
@@ -22,3 +22,31 @@ rl.question('Enter a number: ', (number) => {
 
     printTable();
 });
+
+
+
+
+// const readline = require("readline");
+
+// const r1 = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// r1.question("Enter a number: ", (number) => {
+//     let i = 1;
+
+//     const printTable = () => {
+//         console.log(number * i);
+//         i++;
+
+//         if (i <= 10) {
+//             setTimeout(printTable, 1000);
+//         } else {
+//             console.log("Done");
+//             r1.close();
+//         }
+//     }
+
+//     printTable();
+// })
