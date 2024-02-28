ddb7d91f37c5dce57567a086643b69cd8fb8d41f
anonys6 <shakibalam998@gmail.com>
2024-02-28 08:29:51 +0530

    done 1

--- 1_hello_world.js
+++ 1_hello_world.js
@@ -18,3 +18,16 @@ server.listen(port, hostname, () => {
     
 
 */
+
+
+
+// const http = require("http");
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain");;
+//     res.end("Hello world");
+// });
+
+// server.listen(port);
