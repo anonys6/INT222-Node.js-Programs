5e4993a39c10e6688f287fb360c337f6d6789500
anonys6 <shakibalam998@gmail.com>
2024-04-02 13:30:45 +0530

    last commit

--- CodeWithHarry-Express.js/index.js
+++ CodeWithHarry-Express.js/index.js
@@ -4,7 +4,13 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// const myMiddleWare = (req, res, next) => {
+//     console.log(req);
+//     next();
+// }
+
 app.use(express.static(path.join(__dirname, "public")));
+// app.use(myMiddleWare);
 
 app.get("/", (req, res) => {
     res.send("Hello world");
