5688a78846e9f6ad70960208e91301406fc4fb9b
anonys6 <shakibalam998@gmail.com>
2024-04-05 01:41:35 +0530

    blog project basic structure done

--- CodeWithHarry-Node.js/Blog.js/data/blogs.js
+++ CodeWithHarry-Node.js/Blog.js/data/blogs.js
@@ -1,19 +1,24 @@
 blogs = [
     {
         title: "This is Python",
-        content: "Python uses interpreter"
+        content: "Python uses interpreter",
+        slug: "python-learn"
+
     },
     {
         title: "C++ is the fastest",
-        content: "C++ is a low level language"
+        content: "C++ is a low level language",
+        slug: "cpp-learn"
     },
     {
         title: "Java is good",
-        content: "JVM makes it better"
+        content: "JVM makes it better",
+        slug: "java-learn"
     },
     {
         title: "Rust is the new C/C++",
-        content: "Rust is everything you gonna need"
+        content: "Rust is everything you gonna need",
+        slug: "rust-learn"
     }
 ]
 
--- CodeWithHarry-Node.js/Blog.js/routes/blog.js
+++ CodeWithHarry-Node.js/Blog.js/routes/blog.js
@@ -5,7 +5,18 @@ const blogs = require("../data/blogs");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../template/index.html"));
+    res.sendFile(path.join(__dirname, "../templates/index.html"));
+})
+
+router.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, "../templates/bloghome.html"))
+})
+
+router.get('/blogpost/:slug', (req, res) => {
+    myBlog = blogs.filter((e) => {
+        return req.params.slug;
+    })
+    console.log(myBlog)
 })
 
 module.exports = router;
