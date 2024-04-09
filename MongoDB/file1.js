352d048d0aac90056dadc80e623057bbfff56712
anonys6 <shakibalam998@gmail.com>
2024-04-09 23:11:02 +0530

    adding mongodb file

---
+++ MongoDB/file1.js
@@ -0,0 +1,21 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb://127.0.0.1:27017/Univ";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("Univ");
+    var myobj = [
+        { name: 'Manoj', course: 'Node js', marks: 98, Grade: 'O', Mode: 'Regular' },
+        { name: 'Anu', course: 'React js', marks: 98 },
+        { name: 'Ram', course: 'HTML', marks: 98 },
+        { name: 'Tej', course: 'Angular js', marks: 98 },
+        { name: 'Manoj', course: 'Node js', marks: 98, Grade: 'O', Mode: 'Regular' },
+    ];
+
+    dbo.collection("Student_data").insertMany(myobj, function (err, data) {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + data.insertedCount);
+        db.close();
+    });
+});
